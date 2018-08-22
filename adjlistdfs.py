22691f77d437fefcb32cc095bffc405df3cb396a
christopherhui <christophergkhui@gmail.com>
2018-08-21 22:14:15 -0700

    ajlist DFS

--- adjlistdfs.py
+++ adjlistdfs.py
@@ -1,18 +1,21 @@
+# Undirected graph DFS using dictionaries to implement val (strings, num, etc.)
 class Graph:
 
-    def __init__(self, V):
-        self.vertices = V
+    def __init__(self):
         self.graph = {}
-        for i in range(self.vertices):
-            self.graph[i] = list()
 
     def add_edge(self, u, v):
-        if u in self.graph and v in self.graph:
+        if u not in self.graph:
+            self.graph[u] = [v]
+        else:
             self.graph[u].append(v)
+        if v not in self.graph:
+            self.graph[v] = [u]
+        else:
             self.graph[v].append(u)
 
     def DFS(self, s):
-        visited = [False] * self.vertices
+        visited = [False] * len(self.graph)
         visited[s] = True
         print(s)
         self.DFSUtil(s, visited)
@@ -26,12 +29,11 @@ class Graph:
                 print(i)
                 self.DFSUtil(i, visited)
 
-g = Graph(4)
+g = Graph()
 g.add_edge(0, 1)
 g.add_edge(0, 2)
 g.add_edge(1, 2)
-g.add_edge(2, 0)
 g.add_edge(2, 3)
 g.add_edge(3, 3)
 
-g.DFS(2)
+g.DFS(1)
