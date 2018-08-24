98ad7df76047f93a5bb59c5a2431503c9acfaee3
christopherhui <christophergkhui@gmail.com>
2018-08-24 14:07:39 -0700

    comparison based sorting using bubble

--- largestnuminarray.py
+++ largestnuminarray.py
@@ -2,23 +2,19 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     aList = input().split()
-    createNum = ''
     if len(aList) == 1:
         print(aList[0])
     else:
-        firstNum = int(aList[0] + aList[1])
-        secondNum = int(aList[1] + aList[0])
-        if firstNum > secondNum:
-            createNum += str(firstNum)
-        else:
-            createNum += str(secondNum)
-        for i in range(2, len(aList)):
-            firstNum = int(createNum + aList[i])
-            secondNum = int(aList[i] + createNum)
-            if firstNum > secondNum:
-                createNum = str(firstNum)
-            else:
-                createNum = str(secondNum)
-    print(int(createNum))
+        for z in range(0, len(aList)):
+            for i in range(0, len(aList) - 1):
+                firstNum = int(aList[i] + aList[i + 1])
+                secondNum = int(aList[i + 1] + aList[i])
+                if firstNum > secondNum:
+                    temp = aList[i]
+                    aList[i] = aList[i + 1]
+                    aList[i + 1] = temp
 
-
+    str = ''
+    for j in range(len(aList) - 1, -1, -1):
+        str += aList[j]
+    print(int(str))
